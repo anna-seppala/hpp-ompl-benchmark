50dc19b62924c171f591afaf7fc4f94097c70ac6
Akseppal <seppala@laas.fr>
2016-02-23 15:13:47 +0100

    create benchmarkData

--- benchmarks.py
+++ benchmarks.py
@@ -2,13 +2,13 @@
 # name = Abstract
 # robot = Absrtact_robot
 # world = Abstract_env
-
+from math import pi
 from hpp.corbaserver.hpp_ompl_benchmark.Abstract import Robot
 robot = Robot('ompl')
 from hpp.corbaserver import ProblemSolver
 ps = ProblemSolver (robot)
 from hpp.gepetto import ViewerFactory
-from math import pi
+
 r = ViewerFactory (ps)
 r.loadObstacleModel("hpp-ompl-benchmark", "Abstract_env", "env")
 r (robot.getCurrentConfig ())
@@ -24,54 +24,64 @@ ps.addGoalConfig (q_goal)
 
 from hpp.corbaserver import Benchmark
 benchmark = Benchmark (robot.client, robot, ps)
-benchmark.seedRange = range (10)
-benchmark.iterPerCase = 2
+benchmark.seedRange = range (3)
+benchmark.iterPerCase = 3 
 results = benchmark.do()
 
 #------------------------------------------------------------------
 # name = cubicles
 # robot = cubicles_robot
 # world = cubicles_env
-
+from math import pi
 from hpp.corbaserver.hpp_ompl_benchmark.cubicles import Robot
 robot = Robot('ompl')
 from hpp.corbaserver import ProblemSolver
 ps = ProblemSolver (robot)
 from hpp.gepetto import ViewerFactory
+
 r = ViewerFactory (ps)
 r.loadObstacleModel("hpp-ompl-benchmark", "cubicles_env", "env")
 r (robot.getCurrentConfig ())
 v = r.createRealClient ()
 
-q_init = [-0.0496, -0.4062, 0.7057, 1 , 0, 0, 0]
-q_goal = [2.0, -0.4062, 0.7057, 1, 0, 0, 0]
+q_init = [0, 0, 0, 1, 0, 0, 0]
+q_goal = [5.3, 0.5, 0, 1, 0, 0, 0]
+#q_init = [-0.0496, -0.4062, 0.7057, 1 , 0, 0, 0]
+#q_goal = [2.0, -0.4062, 0.7057, 1, 0, 0, 0]
 
-robot.setJointBounds ('base_joint_xyz', [-5.5, 6.5, -12, 0, -5, 6])
-robot.setJointBounds ('base_joint_SO3', [-pi, pi, -pi, pi, -pi, pi, -pi, pi])
+robot.setJointBounds ('base_joint_xyz', [-12, 8, -0.5, 1.5, -5, 13])
+#robot.setJointBounds ('base_joint_SO3', [-pi, pi, -pi, pi, -pi, pi, -pi, pi])
 
 ps.setInitialConfig (q_init)
 ps.addGoalConfig (q_goal)
+
+from hpp.corbaserver import Benchmark
+benchmark = Benchmark (robot.client, robot, ps)
+benchmark.seedRange = range (10)
+benchmark.iterPerCase = 10 
+results = benchmark.do()
 
 #------------------------------------------------------------------
 # name = pipedream
 # robot = ring
 # world = pipedream_env
-
+from math import pi
 from hpp.corbaserver.hpp_ompl_benchmark.pipedream import Robot
 robot = Robot('ompl')
 from hpp.corbaserver import ProblemSolver
 ps = ProblemSolver (robot)
 from hpp.gepetto import ViewerFactory
+
 r = ViewerFactory (ps)
 r.loadObstacleModel("hpp-ompl-benchmark", "pipedream_env", "env")
 r (robot.getCurrentConfig ())
 v = r.createRealClient ()
 
-q_init = [-0.0496, -0.4062, 0.7057, 1 , 0, 0, 0]
-q_goal = [2.0, -0.4062, 0.7057, 1, 0, 0, 0]
+q_init = [0.8,0.3,1.2,0.707,0.707,0,0]
+q_goal = [0.3,-0.8,0.9,0,1,0,0]
 
 robot.setJointBounds ('base_joint_xyz', [-5.5, 6.5, -12, 0, -5, 6])
-robot.setJointBounds ('base_joint_SO3', [-pi, pi, -pi, pi, -pi, pi, -pi, pi])
+# robot.setJointBounds ('base_joint_SO3', [-pi, pi, -pi, pi, -pi, pi, -pi, pi])
 
 ps.setInitialConfig (q_init)
 ps.addGoalConfig (q_goal)
