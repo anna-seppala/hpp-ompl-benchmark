d880c2ef233ca93ae513eb133a48ca157213fa52
Pierre Fernbach <pierre.fernbach@laas.fr>
2016-02-29 21:44:28 +0100

    adjust bound and configuration value according to OMPL configs files

--- benchmarks2.py
+++ benchmarks2.py
@@ -1,38 +1,5 @@
-# Script to solve ompl problem in hpp:
-# name = Abstract
-# robot = Absrtact_robot
-# world = Abstract_env
-from math import pi
-from hpp.corbaserver.hpp_ompl_benchmark.Abstract import Robot
-robot = Robot('ompl')
-from hpp.corbaserver import ProblemSolver
-ps = ProblemSolver (robot)
-from hpp.gepetto import Viewer
-
-v = Viewer (ps)
-v.loadObstacleModel("hpp-ompl-benchmark", "Abstract_env", "env")
 
 
-
-q_init = [2.1, -5.81216, -1, 1 , 0, 0, 0]
-q_goal = [-3, -4.3, 1, 0.7071, 0.7071, 0, 0]
-v(q_init)
-
-robot.setJointBounds ('base_joint_xyz', [-5.5, 6.5, -12, 0, -5, 6])
-# robot.setJointBounds ('base_joint_SO3', [-pi, pi, -pi, pi, -pi, pi, -pi, pi])
-ps.setInitialConfig (q_init)
-ps.addGoalConfig (q_goal)
-
-from hpp.corbaserver import Benchmark
-benchmark = Benchmark (robot.client, robot, ps)
-ps.client.problem.setRandomSeed(1927402002)
-benchmark.seedRange = range (50)
-benchmark.iterPerCase = 1
-results = benchmark.do()
-
-benchmark.writeDatabase("dbHPP.db","abstract","abstractLogs.log",True)
-
-#------------------------------------------------------------------
 # name = cubicles
 # robot = cubicles_robot
 # world = cubicles_env
@@ -48,12 +15,12 @@ v.loadObstacleModel("hpp-ompl-benchmark", "cubicles_env", "env")
 
 
 
-q_init = [0, 0, 0, 1, 0, 0, 0]
-q_goal = [5.3, 0.5, 0, 1, 0, 0, 0]
+q_init = [-0.126, -1.792, -1.0317, 1, 0, 0, 0]
+q_goal = [5.08, -1.792, -1.0317, 1, 0, 0, 0]
 #q_init = [-0.0496, -0.4062, 0.7057, 1 , 0, 0, 0]
 #q_goal = [2.0, -0.4062, 0.7057, 1, 0, 0, 0]
 
-robot.setJointBounds ('base_joint_xyz', [-12, 8, -0.5, 1.5, -5, 13])
+robot.setJointBounds ('base_joint_xyz', [-12.92, 8.11, -2.565, 3.14, -5.845, 13.5])
 #robot.setJointBounds ('base_joint_SO3', [-pi, pi, -pi, pi, -pi, pi, -pi, pi])
 
 ps.setInitialConfig (q_init)
@@ -61,7 +28,7 @@ ps.addGoalConfig (q_goal)
 
 from hpp.corbaserver import Benchmark
 benchmark = Benchmark (robot.client, robot, ps)
-ps.client.problem.setRandomSeed(3530408688)
+#ps.client.problem.setRandomSeed(3530408688)
 benchmark.seedRange = range (50)
 benchmark.iterPerCase = 1
 results = benchmark.do()
@@ -81,10 +48,10 @@ from hpp.gepetto import Viewer
 v = Viewer (ps)
 v.loadObstacleModel("hpp-ompl-benchmark", "pipedream_env", "env")
 
-q_init = [2,-0,0.75,0.707,0.707,0,0]
-q_goal = [1.6,-0.4,0.7,0,1,0,0]
+q_init = [2.02,-0.0912,0.786,0.707,0.707,0,0]
+q_goal = [1.587,-0.345,0.634,1,0,0,0]
 
-robot.setJointBounds ('base_joint_xyz', [1, 2.5, -1, 0.5, 0, 1.7])
+robot.setJointBounds ('base_joint_xyz', [1.08, 2.43, -0.824, 0.28, 0.3, 1.7])
 # robot.setJointBounds ('base_joint_SO3', [-pi, pi, -pi, pi, -pi, pi, -pi, pi])
 
 ps.setInitialConfig (q_init)
@@ -98,4 +65,41 @@ benchmark.iterPerCase = 1
 results = benchmark.do()
 
 benchmark.writeDatabase("dbHPP.db","pipedream","pipedreamLogs.log",True)
+
+
+#------------------------------------------------------------------
+# Script to solve ompl problem in hpp:
+# name = Abstract
+# robot = Absrtact_robot
+# world = Abstract_env
+from math import pi
+from hpp.corbaserver.hpp_ompl_benchmark.Abstract import Robot
+robot = Robot('ompl')
+from hpp.corbaserver import ProblemSolver
+ps = ProblemSolver (robot)
+from hpp.gepetto import Viewer
+
+v = Viewer (ps)
+v.loadObstacleModel("hpp-ompl-benchmark", "Abstract_env", "env")
+
+
+
+q_init = [2.158, -4.9, -1.524, 1 , 0, 0, 0]
+q_goal = [-3.07, -4, 0.305, 0.7071, 0.7071, 0, 0]
+v(q_init)
+
+robot.setJointBounds ('base_joint_xyz', [-5.92, 6.09, -11.9, 0.1, -5.64, 6.35])
+# robot.setJointBounds ('base_joint_SO3', [-pi, pi, -pi, pi, -pi, pi, -pi, pi])
+ps.setInitialConfig (q_init)
+ps.addGoalConfig (q_goal)
+
+from hpp.corbaserver import Benchmark
+benchmark = Benchmark (robot.client, robot, ps)
+ps.client.problem.setRandomSeed(1927402002)
+benchmark.seedRange = range (50)
+benchmark.iterPerCase = 1
+results = benchmark.do()
+
+benchmark.writeDatabase("dbHPP.db","abstract","abstractLogs.log",True)
+
 
